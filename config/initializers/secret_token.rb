c926fbaf588675af6db4c1dcfc863b8dc82ae50d
unknown <eliran@eliran-PC.(none)>
2012-11-30 21:58:05 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TApp::Application.config.secret_token = 'c2b400c187d78a76d7a2af15c6aa1afcd828ed28c92e2358143fffd04d4af2b66ae6dfc50e2c940df971a7ac1198c2c2a8cf1283edc0d195fe9a511368785f7d'
